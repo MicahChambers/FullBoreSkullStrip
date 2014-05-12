5dab0965b76b0391a4e29e424016b109e0aa53b2
Micah Chambers (troller) <micahcc@ucla.edu>
2014-05-12 14:29:13 -0700

    ordered rigid initialization now works...

--- src/FullBoreSkullStrip.cpp
+++ src/FullBoreSkullStrip.cpp
@@ -23,6 +23,9 @@ along with the Neural Programs Library.  If not, see
 #include "registerIO.h"
 RegisterIO REG;
 
+
+
+#include <itkNearestNeighborInterpolateImageFunction.h>
 #include "itkGradientDescentOptimizer.h"
 #include <itkBSplineTransform.h>
 #include "itkMutualInformationImageToImageMetric.h"
@@ -38,25 +41,33 @@ RegisterIO REG;
 #include "itkVersorRigid3DTransform.h"
 #include "itkCenteredVersorTransformInitializer.h"
 
+#include <itkImageFileWriter.h>
+#include <itkImageFileReader.h>
+
 #include <itkDiscreteGaussianImageFilter.h>
 #include <itkLBFGSBOptimizer.h>
 
+#include <cmath>
+
 using namespace std;
 typedef itk::Image<float,3> ImageT;
+typedef itk::Image<short,3> LImageT;
 
-void bSplineReg(itk::BSplineTransform<double, 3, 3>::Pointer tfm,
+const double PI = acos(-1);
+
+double bSplineReg(itk::BSplineTransform<double, 3, 3>::Pointer tfm,
 			ImageT::Pointer source, ImageT::Pointer target, 
 			double sd, bool samecontrast,
 			int nstep, double minstep, double maxstep,
 			int nbins, double relax, int nsamp, double TOL);
 
-void affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
+double affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
 			ImageT::Pointer source, ImageT::Pointer target, 
 			double sd, bool samecontrast,
 			int nstep, double minstep, double maxstep,
 			int nbins, double relax, int nsamp, double TOL);
 
-void rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
+double rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
 			ImageT::Pointer source, ImageT::Pointer target, 
 			double sd, bool samecontrast,
 			int nstep, double minstep, double maxstep,
@@ -65,6 +76,39 @@ void rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
 ImageT::Pointer apply(itk::Transform<double,3,3>::Pointer tfm, 
 		ImageT::Pointer source, ImageT::Pointer target);
 
+LImageT::Pointer applyNN(itk::Transform<double,3,3>::Pointer tfm, 
+		LImageT::Pointer source, ImageT::Pointer target);
+
+/* Helper function to write an image "out" to prefix + filename */
+template <typename T, typename R = T>
+void writeImage(std::string name, typename T::Pointer in);
+
+template <typename T, typename R = T>
+void writeImage( typename T::Pointer in, std::string name);
+
+template <typename T>
+typename T::Pointer readImage(std::string name);
+
+struct ParamLessEqual {
+	bool operator()(const itk::VersorRigid3DTransform<double>::ParametersType& lhs,
+			const itk::VersorRigid3DTransform<double>::ParametersType& rhs)  const
+	{
+		const double TOL = 0.0000001;
+		for(size_t ii=0 ;ii<lhs.GetSize(); ii++) {
+			if(rhs[ii] - lhs[ii]>TOL) {
+				return true;
+			}
+			if(lhs[ii] - rhs[ii]>TOL) {
+				return false;
+			}
+		}
+		return false;
+	}
+};
+
+/******************************************
+ * Main
+ ******************************************/
 int main(int argc, char** argv)
 {
 	try {
@@ -77,29 +121,180 @@ int main(int argc, char** argv)
 
 	TCLAP::ValueArg<string> a_in("i", "input", "Input Image",
 			true, "", "image", cmd);
-	TCLAP::ValueArg<string> a_out("o", "out", "Output Image",
+	TCLAP::ValueArg<string> a_atlas("a", "atlas", "Atlas with same modality as input", 
 			true, "", "image", cmd);
+	TCLAP::ValueArg<string> a_atlas_label("m", "atlas-label", 
+			"Brain-mask image in same space as atlas", true, "", "image", cmd);
+	TCLAP::ValueArg<string> a_outl("o", "output", "Output brain mask in same "
+			"space as input", false, "", "image", cmd);
+	TCLAP::ValueArg<string> a_out("O", "output-fit", "Output brain image after "
+			"transformation, in same space as input", false, "", "image", cmd);
 	
-	std::vector<string> allowed({"rect","tukey", "hamming", "hann"});
-	TCLAP::ValuesConstraint<string> allowedVals( allowed );
-	TCLAP::ValueArg<string> a_window("w", "window", "Window function.",
-			false, "rect", &allowedVals, cmd);
-
-	TCLAP::MultiArg<int> a_size("z", "size", "Dimensions, first arg is "
-			"x, second is y, third is z.", false, "int", cmd);
-	TCLAP::MultiArg<double> a_space("s", "spacing", "Spacing, first arg is "
-			"x spacing, second is y spacing, third is z.", false, "step", cmd);
-	TCLAP::ValueArg<size_t> a_padsize("p", "padding", "Minimum amount of zero "
-			"padding, helpful during registration where you don't want stuff "
-			"falling out of the field of view", false, 0, "padsize", cmd);
+	TCLAP::SwitchArg a_moments("M", "moments-align", "Moments align first.", cmd);
+	
+	TCLAP::MultiArg<double> a_rigid_smooth("R", "rigid-smooth",
+			"Gaussian smoothing standard deviation during rigid "
+			"registration. Provide mutliple to schedule mutli-resolution "
+			"registration strategy.", false, "double", cmd);
+	TCLAP::MultiArg<double> a_affine_smooth("A", "affine-smooth",
+			"Gaussian smoothing standard deviation during affine "
+			"registration. Provide mutliple to schedule mutli-resolution "
+			"registration strategy.", false, "double", cmd);
+	TCLAP::MultiArg<double> a_bspline_smooth("B", "bspline-smooth",
+			"Gaussian smoothing standard deviation during bspline "
+			"registration. Provide mutliple to schedule mutli-resolution "
+			"registration strategy.", false, "double", cmd);
+	
+	TCLAP::SwitchArg  a_debug("D", "debug",
+			"Write out intermediate images in the current directory "
+			"(warning may overwrite stuff)", cmd);
+	
 	cmd.parse(argc, argv);
 
+	auto input = readImage<ImageT>(a_in.getValue());
+	auto atlas = readImage<ImageT>(a_atlas.getValue());
+	auto labelmap = readImage<LImageT>(a_atlas_label.getValue());
+	
+	vector<double> rigid_smooth(a_rigid_smooth.getValue());
+	vector<double> affine_smooth(a_affine_smooth.getValue());
+	vector<double> bspline_smooth(a_bspline_smooth.getValue());
+
+	if(!a_rigid_smooth.isSet()) {
+		rigid_smooth.resize(3);
+		rigid_smooth[0] = 3;
+		rigid_smooth[1] = 1.5;
+		rigid_smooth[2] = 0.5;
+	}
+	
+	if(!a_affine_smooth.isSet()) {
+		affine_smooth.resize(3);
+		affine_smooth[0] = 3;
+		affine_smooth[1] = 1.5;
+		affine_smooth[2] = 0.5;
+	}
+	
+	if(!a_bspline_smooth.isSet()) {
+		bspline_smooth.resize(3);
+		bspline_smooth[0] = 3;
+		bspline_smooth[1] = 1.5;
+		bspline_smooth[2] = 0.5;
+	}
+
+	auto rigid = itk::VersorRigid3DTransform<double>::New();
+	if(a_moments.isSet()) {
+		auto init = itk::CenteredVersorTransformInitializer<ImageT, ImageT>::New();
+		init->SetTransform(rigid);
+		init->SetFixedImage(input);
+		init->SetMovingImage(atlas);
+		init->MomentsOn();
+		init->InitializeTransform();
+	}
+	
+	itk::Vector<double,3> xaxis((double)0);
+	itk::Vector<double,3> yaxis((double)0);
+	itk::Vector<double,3> zaxis((double)0);
+	itk::Vector<double,3> zero((double)0);
+	xaxis[0] = 1;
+	yaxis[1] = 1;
+	zaxis[2] = 1;
+
+	auto xrigid = itk::VersorRigid3DTransform<double>::New();
+	auto yrigid = itk::VersorRigid3DTransform<double>::New();
+	auto zrigid = itk::VersorRigid3DTransform<double>::New();
+	xrigid->SetFixedParameters(rigid->GetFixedParameters());
+	yrigid->SetFixedParameters(rigid->GetFixedParameters());
+	zrigid->SetFixedParameters(rigid->GetFixedParameters());
+	
+	xrigid->SetRotation(xaxis, PI/2.);
+	yrigid->SetRotation(yaxis, PI/2.);
+	zrigid->SetRotation(zaxis, PI/2.);
+	xrigid->SetTranslation(zero);
+	yrigid->SetTranslation(zero);
+	zrigid->SetTranslation(zero);
+
+	/* Rigid registration, try all different directions */
+	double bestval = 0;
+	auto bestparams = rigid->GetParameters();
+	std::set<itk::VersorRigid3DTransform<double>::ParametersType, ParamLessEqual> tested;
+	for(int xx=0; xx < 4; xx++) {
+		rigid->Compose(xrigid, true); // rotate 90degress
+		for(int yy=0; yy < 4; yy++) {
+			rigid->Compose(yrigid, true); //rotate 90 degress
+			for(int zz=0; zz < 4; zz++) {
+				rigid->Compose(zrigid, true); // rotate 90 degrees
+
+				auto it = tested.insert(rigid->GetParameters());
+				if(!it.second) {
+					cerr << xx << "," << yy << "," << zz << endl;
+					cerr << "Count > 0" << endl;
+					continue;
+				}
+
+//				cerr << rigid->GetTranslation() << endl;
+//				cerr << rigid->GetMatrix() << endl;
+				cerr << rigid->GetParameters() << endl;
+				// perform full registration
+				double val = rigidReg(rigid, atlas, input, 4, 
+						true, 100, 0.01, 1, 0, 0.4, 0, 0.01);
+//				cerr << rigid->GetTranslation() << endl;
+//				cerr << rigid->GetMatrix() << endl;
+//				cerr << rigid->GetParameters() << endl;
+				if(val > bestval) {
+					bestval = val;
+					bestparams = rigid->GetParameters();
+				}
+			}
+		}
+	}
+	
+	// perform real rigid registration with the best parameters from above
+	rigid->SetParameters(bestparams);
+	cerr << rigid->GetTranslation() << endl;
+	cerr << rigid->GetMatrix() << endl;
+	cerr << rigid->GetParameters() << endl;
+	// perform full registration
+	for(size_t ii=0; ii<rigid_smooth.size(); ii++) {
+		rigidReg(rigid, atlas, input, rigid_smooth[ii], 
+				true, 1000, 0.01, 1, 0, 0.4, 0, 0.01);
+	}
+	cerr << rigid->GetTranslation() << endl;
+	cerr << rigid->GetMatrix() << endl;
+	cerr << rigid->GetParameters() << endl;
+
+	/* Affine registration */
+	auto affine = itk::AffineTransform<double, 3>::New();
+	affine->SetTranslation(rigid->GetTranslation());
+	affine->SetMatrix(rigid->GetMatrix());
+	affine->SetCenter(rigid->GetCenter());
+
+	for(int ii=0; affine_smooth.size(); ii++) {
+		affineReg(affine, atlas, input, affine_smooth[ii], 
+				true, 1000, 0.01, 1, 0, 0.4, 0, 0.01);
+	}
+
+	atlas = apply(affine.GetPointer(), atlas, input);
+	labelmap = applyNN(affine.GetPointer(), labelmap, input);
+	
+	/* BSpline Registration */
+	auto bspline = itk::BSplineTransform<double, 3, 3>::New();
+	for(int ii=0; bspline_smooth.size(); ii++) {
+		bSplineReg(bspline, atlas, input, bspline_smooth[ii], 
+				true, 1000, 0.01, 1, 0, 0.4, 0, 0.01);
+	}
+	atlas = apply(bspline.GetPointer(), atlas, input);
+	labelmap = applyNN(bspline.GetPointer(), labelmap, input);
+
+	writeImage<ImageT>(a_out.getValue(), atlas);
+	writeImage<LImageT>(a_outl.getValue(), labelmap);
 	
 	} catch (TCLAP::ArgException &e)  // catch any exceptions
 	{ std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
 	return 0;
 }
 
+/******************************************
+ * Functions
+ ******************************************/
 template <typename T>
 typename T::Pointer gaussianSmooth(typename T::Pointer in, double stddev)
 {
@@ -111,7 +306,7 @@ typename T::Pointer gaussianSmooth(typename T::Pointer in, double stddev)
 	return smoother->GetOutput();
 }
 
-void bSplineReg(itk::BSplineTransform<double, 3, 3>::Pointer tfm,
+double bSplineReg(itk::BSplineTransform<double, 3, 3>::Pointer tfm,
 			ImageT::Pointer source, ImageT::Pointer target, 
 			double sd, bool samecontrast,
 			int nstep, double minstep, double maxstep,
@@ -155,23 +350,22 @@ void bSplineReg(itk::BSplineTransform<double, 3, 3>::Pointer tfm,
 	reg->SetMovingImage(source);
 	reg->SetFixedImageRegion(target->GetLargestPossibleRegion());
   
+	double value = INFINITY;
 	try {
 		std::cerr << "Performing Rigid Registration..."; 
 		reg->Update();
-		std::cerr << " (" 
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< " -> "
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< ") " << opt->GetStopConditionDescription() << std::endl;
+		value = reg ->GetMetric()->GetValue(reg->GetLastTransformParameters());
+		cerr << " (" << value << ") " << opt->GetStopConditionDescription() << endl;
 	} catch( itk::ExceptionObject & err ) {
 		std::cerr<< "ExceptionObject" << std::endl << err << std::endl;
-		return;
+		return -INFINITY;
 	}
 
 	tfm->SetParameters(reg->GetLastTransformParameters());
+	return -value;
 }
 
-void affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
+double affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
 			ImageT::Pointer source, ImageT::Pointer target, 
 			double sd, bool samecontrast,
 			int nstep, double minstep, double maxstep,
@@ -188,6 +382,18 @@ void affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
 //	auto opt = itk::RegularStepGradientDescentOptimizer::New();
 //	auto opt = itk::VersorRigid3DTransformOptimizer::New();
 	auto opt = itk::LBFGSBOptimizer::New();
+	itk::LBFGSBOptimizer::BoundSelectionType bsel(tfm->GetNumberOfParameters());
+  	itk::LBFGSBOptimizer::BoundValueType bnull(tfm->GetNumberOfParameters());
+	bsel.Fill( 0 );
+	bnull.Fill( 0.0 );
+	opt->SetBoundSelection(bsel);
+	opt->SetUpperBound(bnull);
+	opt->SetLowerBound(bnull);
+	opt->SetCostFunctionConvergenceFactor( 1e+7);
+	opt->SetProjectedGradientTolerance( 1e-6 );
+	opt->SetMaximumNumberOfIterations(200);
+	opt->SetMaximumNumberOfEvaluations(30);
+	opt->SetMaximumNumberOfCorrections(5);
 //	opt->SetMinimumStepLength(minstep);
 //	opt->SetMaximumStepLength(maxstep);
 //	opt->SetRelaxationFactor(relax);
@@ -223,35 +429,27 @@ void affineReg(itk::AffineTransform<double, 3>::Pointer tfm,
 	reg->SetMovingImage(source);
 	reg->SetFixedImageRegion(target->GetLargestPossibleRegion());
   
+	double value = INFINITY;
 	try {
 		std::cerr << "Performing Rigid Registration..."; 
 		reg->Update();
-		std::cerr << " (" 
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< " -> "
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< ") " << opt->GetStopConditionDescription() << std::endl;
+		value = reg ->GetMetric()->GetValue(reg->GetLastTransformParameters());
+		cerr << " (" << value << ") " << opt->GetStopConditionDescription() << endl;
 	} catch( itk::ExceptionObject & err ) {
 		std::cerr<< "ExceptionObject" << std::endl << err << std::endl;
-		return;
+		return -INFINITY;
 	}
 
 	tfm->SetParameters(reg->GetLastTransformParameters());
+	return -value;
 }
 
-void rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
+double rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
 		itk::Image<float,3>::Pointer source, itk::Image<float,3>::Pointer target, 
 		double sd, bool samecontrast,
 		int nstep, double minstep, double maxstep,
 		int nbins, double relax, int nsamp, double TOL)
 {
-	auto init = itk::CenteredVersorTransformInitializer<ImageT, ImageT>::New();
-	init->SetTransform(tfm);
-	init->SetFixedImage(target);
-	init->SetMovingImage(source);
-  	init->MomentsOn();
-  	init->InitializeTransform();
-	
 	if(sd > 0) {
 		source = gaussianSmooth<ImageT>(source, sd);
 		target = gaussianSmooth<ImageT>(target, sd);
@@ -297,20 +495,19 @@ void rigidReg(itk::VersorRigid3DTransform<double>::Pointer tfm,
 	reg->SetMovingImage(source);
 	reg->SetFixedImageRegion(target->GetLargestPossibleRegion());
   
+	double value = INFINITY;
 	try {
 		std::cerr << "Performing Rigid Registration..."; 
 		reg->Update();
-		std::cerr << " (" 
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< " -> "
-				<< reg ->GetMetric()->GetValue(reg->GetLastTransformParameters()) 
-				<< ") " << opt->GetStopConditionDescription() << std::endl;
+		value = reg ->GetMetric()->GetValue(reg->GetLastTransformParameters());
+		cerr << " (" << value << ") " << opt->GetStopConditionDescription() << endl;
 	} catch( itk::ExceptionObject & err ) {
 		std::cerr<< "ExceptionObject" << std::endl << err << std::endl;
-		return;
+		return -INFINITY;
 	}
 
 	tfm->SetParameters(reg->GetLastTransformParameters());
+	return -value;
 }
 
 
@@ -329,3 +526,53 @@ ImageT::Pointer apply(itk::Transform<double,3,3>::Pointer tfm,
 
 	return resample->GetOutput();
 }
+
+LImageT::Pointer applyNN(itk::Transform<double,3,3>::Pointer tfm, 
+		LImageT::Pointer source, ImageT::Pointer target)
+{
+  	auto resample = itk::ResampleImageFilter<LImageT, LImageT>::New();
+	auto interp = itk::NearestNeighborInterpolateImageFunction<LImageT, double>::New();
+	resample->SetTransform(tfm);
+	resample->SetInput(source);
+	resample->SetInterpolator(interp);
+	resample->SetSize(target->GetLargestPossibleRegion().GetSize());
+	resample->SetOutputOrigin(target->GetOrigin());
+	resample->SetOutputSpacing(target->GetSpacing());
+	resample->SetOutputDirection(target->GetDirection());
+	resample->SetDefaultPixelValue(0);
+	resample->Update();
+
+	return resample->GetOutput();
+}
+
+/* Helper function to write an image "out" to prefix + filename */
+template <typename T, typename R = T>
+void writeImage(std::string name, typename T::Pointer in)
+{
+	auto cast = itk::CastImageFilter<T, R>::New();
+	cast->SetInput(in);
+	cast->Update();
+
+    typename itk::ImageFileWriter<T>::Pointer writer;
+    writer = itk::ImageFileWriter<T>::New();
+    writer->SetFileName(name);
+    writer->SetInput(cast->GetOutput());
+    writer->Update();
+}
+
+template <typename T, typename R = T>
+void writeImage( typename T::Pointer in, std::string name)
+{
+	writeImage<T, R>(name, in);
+}
+
+template <typename T>
+typename T::Pointer readImage(std::string name)
+{
+    typename itk::ImageFileReader<T>::Pointer reader;
+    reader = itk::ImageFileReader<T>::New();
+    reader->SetFileName( name );
+    reader->Update();
+    return reader->GetOutput();
+}
+
